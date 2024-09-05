f78b279e0ecb7dc5d8b33ecd31739d3ae789abb5
kkoli <kajal_koli@bmc.com>
2024-09-05 12:28:33 +0530

    ftp code

--- azure-deveops-ispw-operation-task/index.ts
+++ azure-deveops-ispw-operation-task/index.ts
@@ -1,240 +1,94 @@
 import tl = require("azure-pipelines-task-lib/task");
-const ActionFactory = require("./actions/ActionFactory");
-const Input = require("./transferObj/input");
-const TaskResponse = require("./transferObj/TaskResponse");
-const BuildResponse = require("./transferObj/BuildResponse");
-const SummarizeResponse = require("./services/SummarizeResponse");
-import { getInputs } from "./actions/sync/input-helper";
-import {
-  execISPWSync,
-  getISPWCLIPath,
-} from "./actions/sync/ispw-command-helper";
+import path = require("path");
 
-const RestUtils = require("./utils/RestUtils");
-const CertificateUtils = require("./utils/CertificateUtils");
-const polling_interval: number = 2000;
+import { Client } from 'basic-ftp';
+import * as fs from 'fs';
 
-const SET_STATE_DISPATCHED: string = "Dispatched";
-const SET_STATE_EXECUTING = "Executing";
-const SET_STATE_COMPLETE = "Complete";
-const SET_STATE_CLOSED = "Closed";
-const SET_STATE_FAILED = "Failed";
-const SET_STATE_HELD = "Held";
-const SET_STATE_RELEASED = "Released";
-const SET_STATE_TERMINATED = "Terminated";
-const SET_STATE_WAITING_APPROVAL = "Waiting-Approval";
-const SET_STATE_WAITING_LOCK = "Waiting-Lock";
+//const FtpClient = require('ftp-client');
+//const fs = require('fs');
 
-function isEmpty(str: string) {
-  return !str || str.length === 0;
+const config = {
+
+  //hardcoding values temp
+  host: '#####',
+  port: '21',
+  user: 'administrator',
+  password: '######'
+
 }
 
-function sleep(ms: number) {
-  return new Promise((resolve) => setTimeout(resolve, ms));
-}
+//
 
-async function run() {
-  const connectionId: string | undefined = tl.getInput("connectionId", true);
+const uploadFile = async () => {
+  // FTP server details
+  const server = '#####'; // Replace with your FTP server
+  const user = 'administrator'; // Replace with your FTP username
+  const password = '#####'; // Replace with your FTP password
+  const remoteDir = 'C:/inetpub/ftproot/ftp_data/from_win10/server-file.txt'; // Replace with your remote directory path
+  const localFilePath = 'C:/ftp-data/server-file.txt'; // Local path to the file to upload
+  // Create a local file to upload
+  //fs.writeFileSync(localFilePath, 'Hello, FTP! This is a test file.');
+
+
   const operationType: string | undefined = tl.getInput("operationType", true);
+
   if (operationType) {
     if (operationType == "CES") {
-      const cesUrl: string | undefined = tl.getInput("cesUrl", true);
-      const action: any | undefined = tl.getInput("action", true);
-      const payload: string | undefined = tl.getInput("request", false);
-      const authenticationType: string | undefined = tl.getInput("authenticationType", true);
-      let certContent: string | undefined = undefined;
-      let cesToken: string | undefined = undefined;
-      let certKey: string | undefined = undefined;
-      let trustAllCerts: boolean = false;
-      const cesUrlObj = new URL(cesUrl as string);
-      if (cesUrlObj.protocol == 'https:') {
-        trustAllCerts = tl.getBoolInput("trustAllCerts");
-        const certUtils = new CertificateUtils();
-        const connectedService = tl.getInputRequired("ConnectedServiceName");
-        const keyvaultName = tl.getInputRequired("keyvaultName");
-        const certificateName = tl.getInputRequired("certificateName");
-        await certUtils.getCertificate(authenticationType, connectedService, keyvaultName, certificateName).then(function(authenticate: Authenticate) {
-          cesToken = authenticate.cesToken;
-          certContent = authenticate.certificate;
-          certKey = authenticate.pkcs;
-        });
-      } else {
-        cesToken = tl.getInput("cesSecretToken");
-      }
-      const buildAutomatically: boolean | undefined = tl.getBoolInput(
-        "buildAutomatically"
-      ); //fetching value from buildAutomatically checkbox
-      const skipWaitingForSetCompletion: boolean | undefined = tl.getBoolInput(
-        "skipWaitingForSetCompletion"
-      );
-      const showResponseBodyInConsole: boolean | undefined = tl.getBoolInput(
-        "showResponseBodyInConsole"
-      );
-      var isValidInput: boolean =
-        connectionId != undefined &&
-        cesUrl != undefined &&
-        action != undefined &&
-        !isEmpty(connectionId) &&
-        !isEmpty(cesUrl);
-      var ispwActions: IspwActions;
-      if (isValidInput) {
-        let connection = connectionId != undefined ? connectionId.split("#") : "";
-        let codePage = connection[1].trim();
-        let conStr = connection[0];
-        let hostPortArr = conStr.split(":");
-        let actionFactory = new ActionFactory();
-        let util = new RestUtils();
-        let sr = new SummarizeResponse();
-        if (!isEmpty(action)) {
-          ispwActions = actionFactory.createObj(action);
-          let input = new Input(
-            hostPortArr[0],
-            hostPortArr[1],
-            codePage,
-            cesUrl,
-            payload,
-            authenticationType,
-            cesToken,
-            certContent,
-            certKey,
-            buildAutomatically,
-            skipWaitingForSetCompletion,
-            showResponseBodyInConsole,
-            trustAllCerts
-          );
-          const respObject = await ispwActions.performAction(input);
-          sr.summarize(respObject, action);
-          if (!skipWaitingForSetCompletion) {
-            let setId = "";
-            let url = "";
-            if (respObject instanceof TaskResponse) {
-              let taskResp = respObject as TaskResponse;
-              setId = taskResp.setId;
-              url = taskResp.url;
-            } else if (respObject instanceof BuildResponse) {
-              let buildResp = respObject as TaskResponse;
-              setId = buildResp.setId;
-              url = buildResp.url;
-            }
-            if (
-              setId &&
-              (respObject instanceof TaskResponse ||
-                respObject instanceof BuildResponse) &&
-              action != "SetOperation"
-            ) {
-              input = new Input(
-                hostPortArr[0],
-                hostPortArr[1],
-                codePage,
-                url,
-                {},
-                authenticationType,
-                cesToken,
-                certContent,
-                certKey,
-                buildAutomatically,
-                skipWaitingForSetCompletion,
-                showResponseBodyInConsole,
-                trustAllCerts
-              );
-              ispwActions = actionFactory.createObj("SetInfo");
-              let i: number = 0;
-              for (; i < 60; i++) {
-                await sleep(polling_interval);
-                const setResponse: IspwResponse =
-                  await ispwActions.performAction(input);
-                let set_obj = setResponse as SetInfoResponse;
-                console.log("Waiting for set to complete...");
-                if (set_obj.state == SET_STATE_FAILED) {
-                  console.log(
-                    "Code Pipeline: Set " + set_obj.setid + " - action [%s] failed.",
-                    action
-                  );
-                  break;
-                } else if (set_obj.state == SET_STATE_TERMINATED) {
-                  console.log(
-                    "Code Pipeline: Set " + set_obj.setid + " - successfully terminated."
-                  );
-                  break;
-                } else if (set_obj.state == SET_STATE_HELD) {
-                  console.log(
-                    "Code Pipeline: Set " + set_obj.setid + " - successfully held."
-                  );
-                  break;
-                } else if (
-                  set_obj.state == SET_STATE_RELEASED ||
-                  set_obj.state == SET_STATE_WAITING_LOCK
-                ) {
-                  console.log(
-                    "Code Pipeline: Set " + set_obj.setid + " - successfully released."
-                  );
-                  break;
-                } else if (set_obj.state == SET_STATE_WAITING_APPROVAL) {
-                  console.log(
-                    "Code Pipeline: In set (" +
-                    set_obj.setid +
-                    ") process, Approval required."
-                  );
-                  break;
-                } else if (
-                  set_obj.state == SET_STATE_CLOSED ||
-                  set_obj.state == SET_STATE_COMPLETE
-                ) {
-                  console.log(
-                    "Code Pipeline: Action " +
-                    util.splitPascalCase(action) +
-                    " completed."
-                  );
-                  break;
-                }
 
-                if (i == 60) {
-                  console.log("Max time out reached.");
-                }
-              }
-            }
-          }
-          if (skipWaitingForSetCompletion) {
-            console.log(
-              "Skip waiting for the completion of the set for this job..."
-            );
-          }
-        }
-      }
-    } else {
+
+    }
+    else {
+
+      const client = new Client();
+
       try {
-        //Get current working directory
-        const curWk = tl.getVariable("Build.SourcesDirectory");
+            // Connect to the FTP server
+            await client.access({
+                host: server,
+                user: user,
+                password: password,
+                secure: false // Set to true if you are using FTPS
+            });
 
-        //Inputs from user
-        const parms = getInputs();
+              console.log('Files in the current directory:');
+              const currentDirFiles = await client.list();
+              currentDirFiles.forEach(file => console.log(file.name));
+              await client.cd('from_win10')
+              //await client.cd('..');
 
-        //Get Topaz Workbench CLI Home Path
-        let clipath = "";
-        try {
-          clipath = await getISPWCLIPath(await parms);
-        } catch (error) {
-          if (error instanceof Error) {
-            throw error;
-          }
-        }
+              // List files in the parent directory
+              console.log('\nFiles in the current directory:');
+              const parentDirFiles = await client.list();
+              parentDirFiles.forEach(file => console.log(file.name));
 
-        //Sync Git to ISPW
-        try {
-          await execISPWSync(clipath, await parms, curWk);
-        } catch (error) {
-          if (error instanceof Error) {
-            throw error;
-          }
-        }
-        console.log("Code Pipeline Sync action is completed.");
+              const remoteDirPath =  await client.pwd() 
+              const path1 = path.join(remoteDirPath, '/server-file.txt');
+              //'/server-file.txt';
+              console.log(remoteDirPath);
+              console.log(path1);
+
+              console.log("downloading file here now");
+              await client.downloadTo(localFilePath, path1);
+              //await client.list()
+              // Change to the remote directory
+              //await client.cd(remoteDir);
+
+              // Upload the file
+              //await client.uploadFrom(localFilePath, 'remote-file.txt'); // Remote file name
+
+              //console.log('File uploaded successfully.');
       } catch (error) {
-        if (error instanceof Error) {
-          tl.setResult(tl.TaskResult.Failed, error.message);
-        }
+      console.error('Error:', error);
+      } finally {
+      client.close();
+      // Clean up local file
+      //fs.unlinkSync(localFilePath);
       }
     }
   }
+    
 }
 
-run();
+  
+// Execute the upload function
+uploadFile();
